fd0ed977d76bc61ad1444632dfab786cb33890ae
Louis <louisthomas.pro@gmail.com>
2020-02-06 00:58:42 +0100

    update app directory in server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(forceSsl);
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/monpanier-front'));
+app.use(express.static(__dirname + '/dist/triathlon-front'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/monpanier-front/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/triathlon-front/index.html'));
 });
 
 // default Heroku PORT
